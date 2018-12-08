597ab35902a30fcb98333eb54bd72e63778e1e64
luis-modesto <30834591+luis-modesto@users.noreply.github.com>
2018-12-08 18:43:27 -0300

    Novas telas

--- Codigo_Fonte/views/TelaCriaBolao.js
+++ Codigo_Fonte/views/TelaCriaBolao.js
@@ -1,4 +1,29 @@
+function addBolaoToDB(nome, campeonato, esporte) {
+	let novoBolao = nome + ';' + campeonato + ';' + esporte +';' + './jogos_' + ';'; 
+	novoBolao += ';';
+	let bolaouser = 'ativo;'
+	// escreve bolao no arquivo bolao
+	DataGetter.getInstance().appendData('bolao', novoBolao);
+	// escerve bolaouser no arquivo boloes_cpfuser
+	DataGetter.getInstance().appendData('boloes', bolaouser);
+}
+
+function createNewBolao(){
+	//Recebe os dados e cria o bolão
+	var nome = document.getElementById('nome').value;
+	var campeonato = document.getElementById('campeonato').value;
+	var esporte = document.getElementById('esporte').value;
+	
+	// Escrever Bolao no Banco
+	//addBolaoToDB(nome, campeonato, esporte);
+
+	//Exibe o bolão criado na tela incial
+	
+}
+
+/*
 import {Apostador} from "../models/Apostador.js";
+
 
 export class TelaCriaBolao(){
 	confirmarCriacaoBolao(){
@@ -6,3 +31,4 @@ export class TelaCriaBolao(){
 		user.criarBolao(document.getElementByid('nome').value, document.getElementById('campeonato').value, document.getElementById('esporte'));
 	}
 }
+*/
